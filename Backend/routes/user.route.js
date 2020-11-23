3ef0798a9c9812a0fd214ec1034c8236aa15bf44
César Cubillos <cesar@soyyo.co>
2020-11-23 14:09:30 -0500

    UPDATED - Se actualiza cors

--- Backend/routes/user.route.js
+++ Backend/routes/user.route.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
+const cors = require("cors");
 
 const userController = require ('../controllers/user.controller'); 
-
+router.use(cors());
 
 /**
  * GET Route to list all Users
